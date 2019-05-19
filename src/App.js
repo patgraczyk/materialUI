7c681a1ba49d245fd69b6b54e97babaa62d17b87
Pat <graczyk.pat@gmail.com>
2019-05-19 16:04:54 +0100

    cards added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from './components/Navbar'
+import Posts from './components/Posts'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
